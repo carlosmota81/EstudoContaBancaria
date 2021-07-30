0dfdf85134873550c05bc7e9003ba8f6d64f139b
Carlos_Mota <carlosmota81@gmail.com>
2021-07-30 20:53:30 -0300

    Estudos sobre Funçoes

---
+++ src/entitis/Account.java
@@ -0,0 +1,51 @@
+package entitis;
+
+public class Account {
+
+    private int number;
+    private String holder;
+    private double balance;
+
+   public Account(int number, String holder){
+
+       this.number = number;
+       this.holder = holder;
+   }
+   public  Account (int number, String holder, double initialDeposit){
+       this.number = number;
+       this.holder = holder;
+       deposit(initialDeposit);
+   }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public void deposit(double amount){
+       balance += amount;
+    }
+    public void withdraw(double amount){
+       balance -= amount + 5.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "number=" + number +
+                ", holder='" + holder + '\'' +
+                ", balance=" + balance +
+                '}'+ String.format("%.2f",balance);
+    }
+}
